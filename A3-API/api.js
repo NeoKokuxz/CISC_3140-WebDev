e61177a26a3491dae6f3a5c38321dc5a426ff26f
NeoKokuxz <neko.kokubyakuin@yahoo.com>
2019-09-19 16:05:39 -0400

    edited

--- A3-API/api.js
+++ A3-API/api.js
@@ -1,8 +1,11 @@
+const apiKey = "hEDf8ZMZl4B5dmxUXlCR0lgnRqKREpT7Ckb6QWFP";
+// const today = new Date();
+// const unit = "&date=";
+var urlPath = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey ;
 
-var url = "https://api.nasa.gov/planetary/apod?api_key=hEDf8ZMZl4B5dmxUXlCR0lgnRqKREpT7Ckb6QWFP";
 
 $.ajax({
-  url: url,
+  url: urlPath,
   success: function(result){
   if("copyright" in result) {
     $("#copyright").text("Image Credits: " + result.copyright);
@@ -10,17 +13,17 @@ $.ajax({
   else {
     $("#copyright").text("Image Credits: " + "Public Domain");
   }
-  
+
   if(result.media_type == "video") {
-    $("#apod_img_id").css("display", "none"); 
+    $("#apod_img_id").css("display", "none");
     $("#apod_vid_id").attr("src", result.url);
   }
   else {
-    $("#apod_vid_id").css("display", "none"); 
+    $("#apod_vid_id").css("display", "none");
     $("#apod_img_id").attr("src", result.url);
   }
   $("#reqObject").text(url);
-  $("#returnObject").text(JSON.stringify(result, null, 4));  
+  $("#returnObject").text(JSON.stringify(result, null, 4));
   $("#apod_explaination").text(result.explanation);
   $("#apod_title").text(result.title);
 }
